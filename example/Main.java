ae767ba4bafffcf10971ee4166eff8fa455d7f9d
irinagvozdeva <121356350+irinagvozdeva@users.noreply.github.com>
2025-12-11 12:26:57 +0300

    Add files via upload

---
+++ example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ToDoList toDoList = new ToDoList("Список дел");
+        toDoList.addNewTask(new Task("Сходить в магазин"));
+        toDoList.addNewTask(new Task("Купить воду"));
+        toDoList.addNewTask(new Task("Купить колечко"));
+        toDoList.addNewTask(new Task("Купить браслетик"));
+        toDoList.addNewTask(new Task("Помыть посуду"));
+        toDoList.addNewTask(new Task("Помыть пол"));
+        toDoList.addNewTask(new Task("Сходить в магазин"));
+
+        System.out.println(toDoList);
+
+        toDoList.completeTask(3);
+        toDoList.completeTask(4);
+        toDoList.completeTask("Помыть пол");
+
+        System.out.println(toDoList);
+
+        toDoList.printCompletedTasks();
+        toDoList.printNotCompletedTasks();
+
+        toDoList.statistics();
+    }
+}
